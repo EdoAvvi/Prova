504803894584ab803b2107118d6325af13a8d590
EdoAvvi <edoavvi@gmail.com>
2018-03-06 17:53:28 +0100

    Primo_commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640,600),"");
+    sf::CircleShape shape;
+    shape.setRadius(60.f);
+    shape.setPosition(40.f, 100.f);
+
+    shape.setFillColor(sf::Color::Cyan);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
