37de39275c5bfef7b686a822be95beefb4164a98
EdoAvvi <edoavvi@gmail.com>
2018-03-06 18:37:37 +0100

    Fix_dimension_of_radious

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(640,600),"");
     sf::CircleShape shape;
-    shape.setRadius(500.f);
+    shape.setRadius(60.f);
     shape.setPosition(40.f, 100.f);
 
     shape.setFillColor(sf::Color::Cyan);
